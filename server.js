3f93a2a23c2da6781eb6371590bec29806dd79d4
Rishitme <rishit.1513071@kiet.edu>
2016-09-24 12:06:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,53 @@ var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
+
+var artilceOne={
+title:"IMAD | Rishit Tandon",
+heading:"This is artcile 3",
+content:"This is article one.This is article one.This is artcile one",
+};
+
+function createTemplate(data)
+{ 
+    var title=data.title;
+    var heading=data.heading;
+    var content=data.content;
+    
+  var htmlTemplate=`
+<html>
+
+<head>
+    
+<link href="/ui/style.css" rel="stylesheet"/>
+
+<title>
+{title}
+</title>
+
+</head>
+
+<body>
+    
+<h1>
+{heading}
+</h1>
+<p>
+{content}
+</p>
+
+
+<script type="text/javascript" src="/ui/main.js">
+        </script>
+</body>
+
+</html>
+`;
+    return htmlTemplate;
+}
+
+
+
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
@@ -11,7 +58,7 @@ app.get('/', function (req, res) {
 
 app.get('/article-one.html',function(req,res)
 {
-    res.sendFile(path.join(__dirname,'ui','article-one.html'));
+    res.send(createTemplate(articleOne))
 });
 
 app.get('/ui/style.css', function (req, res) {
