578fbb336dc91161b1119ee658064f94b964dc96
Rishitme <rishit.1513071@kiet.edu>
2016-09-25 15:08:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,9 @@
 console.log('Loaded!');
 var button=document.getElementById("btn");
+var counter=0;
 button.onclick=function()
 {
     counter=counter+1;
     var span=document.getElementById("sp");
     span.innerHTML=counter.toString();
-}
+};
